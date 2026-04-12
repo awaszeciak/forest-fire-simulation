652162fdc5165a27334b013fc6355fe79acc2269
Adriana <a.waszeciak@gmail.com>
2026-04-13 01:10:08 +0200

    Class simulation update

--- src/pl/forestfire/simulation/Simulation.java
+++ src/pl/forestfire/simulation/Simulation.java
@@ -1,4 +1,61 @@
 package pl.forestfire.simulation;
 
+import pl.forestfire.model.*;
+
+import java.util.Random;
+
 public class Simulation {
+
+    private Forest forest;
+    private Random random = new Random();
+
+    public Simulation(Forest forest) {
+        this.forest = forest;
+    }
+
+    public Forest getForest() {
+        return forest;
+    }
+
+    public void step() {
+        int x = random.nextInt(forest.getWidth());
+        int y = random.nextInt(forest.getHeight());
+
+        Cell cell = forest.getCell(x, y);
+
+        if (cell.getState() == State.BURNING) {
+            double r = random.nextDouble();
+
+            if (r < 0.1) {
+                cell.setState(State.DEAD);
+            }
+        } else {
+            int[][] neighbours = {
+                    {x+1, y},
+                    {x-1, y},
+                    {x, y+1},
+                    {x, y-1}
+            };
+
+            int[] n = neighbours[random.nextInt(neighbours.length)];
+            int nx = n[0];
+            int ny = n[1];
+
+            if (nx >= 0 && nx < forest.getWidth() &&
+                ny >= 0 && ny < forest.getHeight()) {
+
+                Cell neighbour = forest.getCell(nx, ny);
+
+                if (neighbour.getState() == State.SUSPECTED) {
+                    double r = random.nextDouble();
+                    if (r < 1.0) {
+                        neighbour.setState(State.BURNING);
+                    }
+                }
+            }
+        }
+
+    }
+
+
 }
